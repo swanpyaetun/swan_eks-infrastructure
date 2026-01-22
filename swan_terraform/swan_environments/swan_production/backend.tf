a1b11919d17f3b2aac8de2bdef0297c0398b8c02
swanpyaetun <swanpyaetun10@gmail.com>
2026-01-22 21:48:24 +0800

    Add .github/workflows/swan_terraform.yml

--- swan_terraform/swan_environments/swan_production/backend.tf
+++ swan_terraform/swan_environments/swan_production/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket       = "swan-production-terraform-backend"
     key          = "swan_production/terraform.tfstate"
     region       = "ap-southeast-1"
-    use_lockfile = true #s3 state locking
+    use_lockfile = true # s3 state locking
     encrypt      = true
   }
 }
