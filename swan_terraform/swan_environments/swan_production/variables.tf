8b55bae59cb212b3c5f5b8bb90d6dc291e3fa950
swanpyaetun <swanpyaetun10@gmail.com>
2026-01-29 22:43:13 +0800

    Update swan_production/variables.tf

--- swan_terraform/swan_environments/swan_production/variables.tf
+++ swan_terraform/swan_environments/swan_production/variables.tf
@@ -30,12 +30,19 @@ variable "swan_eks_cluster_version" {
 variable "swan_eks_node_groups" {
   type = map(object({
     instance_types = list(string)
-    capacity_type  = string
+    capacity_type  = optional(string)
     scaling_config = object({
       desired_size = number
       min_size     = number
       max_size     = number
     })
+    labels = optional(map(string))
+    taints = optional(list(object({
+      key    = string
+      value  = string
+      effect = string
+    })))
+    tags = optional(map(string))
   }))
 }
 
