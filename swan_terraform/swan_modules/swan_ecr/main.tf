89cfe5b3247288885a0dd06e316e5625f668ad13
swanpyaetun <swanpyaetun10@gmail.com>
2026-02-03 20:06:36 +0800

    Enable force_delete for swan_ecr_repositories [skip ci]

--- swan_terraform/swan_modules/swan_ecr/main.tf
+++ swan_terraform/swan_modules/swan_ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "swan_ecr_repositories" {
-  for_each = toset(var.swan_ecr_repository_names)
-  name     = "${var.swan_ecr_namespace}/${each.value}"
+  for_each     = toset(var.swan_ecr_repository_names)
+  name         = "${var.swan_ecr_namespace}/${each.value}"
+  force_delete = true
 }
 
 resource "aws_ecr_lifecycle_policy" "swan_ecr_lifecycle_policy" {
@@ -13,9 +14,9 @@ resource "aws_ecr_lifecycle_policy" "swan_ecr_lifecycle_policy" {
         rulePriority = 1,
         description  = "Keep last 30 images",
         selection = {
-          tagStatus     = "any",
-          countType     = "imageCountMoreThan",
-          countNumber   = 30
+          tagStatus   = "any",
+          countType   = "imageCountMoreThan",
+          countNumber = 30
         },
         action = {
           type = "expire"
