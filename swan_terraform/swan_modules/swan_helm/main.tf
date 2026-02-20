92b23bfe6f5535b8eb7dda349f81163000e4a63f
swanpyaetun <swanpyaetun11@gmail.com>
2026-02-20 19:52:44 +0800

    Clean up

--- swan_terraform/swan_modules/swan_helm/main.tf
+++ swan_terraform/swan_modules/swan_helm/main.tf
@@ -59,17 +59,3 @@ resource "helm_release" "swan_metrics_server_helm_release" {
   version    = "3.13.0"
   namespace  = "kube-system"
 }
-
-resource "helm_release" "swan_vertical_pod_autoscaler_helm_release" {
-  name       = "vertical-pod-autoscaler"
-  repository = "https://kubernetes.github.io/autoscaler"
-  chart      = "vertical-pod-autoscaler"
-  version    = "0.8.0"
-  namespace  = "kube-system"
-
-  values = [
-    file("${path.module}/swan_values/vertical-pod-autoscaler.yaml")
-  ]
-
-  depends_on = [helm_release.swan_aws_load_balancer_controller_helm_release]
-}
