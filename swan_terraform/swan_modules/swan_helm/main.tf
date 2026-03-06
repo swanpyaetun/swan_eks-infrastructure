0568ae8d43a08b252595d786308ec63c98bbc0dc
swanpyaetun <swanpyaetun11@gmail.com>
2026-03-06 22:13:11 +0800

    Remove lifecycle block in swan_karpenter_helm_release

--- swan_terraform/swan_modules/swan_helm/main.tf
+++ swan_terraform/swan_modules/swan_helm/main.tf
@@ -89,10 +89,4 @@ resource "helm_release" "swan_karpenter_helm_release" {
       swan_karpenter_interruption_sqs_queue_name = var.swan_karpenter_interruption_sqs_queue_name
     })
   ]
-
-  lifecycle {
-    ignore_changes = [
-      metadata
-    ]
-  }
 }
